d24ccff53cba199cf692fcd5e647dd85ab99814a
Michael Haase <darkstar@gernox.de>
2020-06-28 07:02:46 +0200

    Remove postgresql docker

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,15 @@
 #
 # @param keycloak_version
 #
-# @param postgresql_version
-#
 # @param db_user
 #
 # @param db_password
 #
 # @param db_name
+#
+# @param db_host
+#
+# @param db_port
 #
 class gernox_keycloak (
   String $base_url,
@@ -27,11 +29,12 @@ class gernox_keycloak (
   String $admin_password,
 
   String $keycloak_version,
-  String $postgresql_version,
 
   String $db_user,
   String $db_password,
   String $db_name,
+  String $db_host,
+  Integer $db_port,
 
   String $log_level,
 ) {
@@ -43,35 +46,4 @@ class gernox_keycloak (
   Class['::gernox_docker']
   -> Class['::gernox_keycloak::server::images']
   ~> Class['::gernox_keycloak::server::run']
-
-  # Script for database backup
-  file { '/gernox/keycloak_psql_backup.sh':
-    ensure  => present,
-    mode    => '0750',
-    content => template('gernox_keycloak/keycloak_psql_backup.sh.erb'),
-  }
-
-  # Systemd service and timer for database backup
-  file { '/etc/systemd/system/docker-keycloak-postgres-pgdump.service':
-    ensure  => present,
-    content => file('gernox_keycloak/docker-keycloak-postgres-pgdump.service'),
-    notify  => Service['docker-keycloak-postgres-pgdump'],
-  }
-
-  file { '/etc/systemd/system/docker-keycloak-postgres-pgdump.timer':
-    ensure  => present,
-    content => file('gernox_keycloak/docker-keycloak-postgres-pgdump.timer'),
-    notify  => Service['docker-keycloak-postgres-pgdump'],
-  }
-
-  service { 'docker-keycloak-postgres-pgdump':
-    ensure  => running,
-    enable  => true,
-    name    => 'docker-keycloak-postgres-pgdump.timer',
-    require => [
-      File['/gernox/keycloak_psql_backup.sh'],
-      File['/etc/systemd/system/docker-keycloak-postgres-pgdump.timer'],
-      File['/etc/systemd/system/docker-keycloak-postgres-pgdump.service'],
-    ],
-  }
 }
--- manifests/server/run.pp
+++ manifests/server/run.pp
@@ -15,28 +15,27 @@
 #
 # @param keycloak_version
 #
-# @param postgresql_version
-#
 class gernox_keycloak::server::run (
-  String $base_url           = $gernox_keycloak::base_url,
-  Integer $http_port         = $gernox_keycloak::http_port,
+  String $base_url         = $gernox_keycloak::base_url,
+  Integer $http_port       = $gernox_keycloak::http_port,
 
-  String $admin_user_name    = $gernox_keycloak::admin_user_name,
-  String $admin_password     = $gernox_keycloak::admin_password,
+  String $admin_user_name  = $gernox_keycloak::admin_user_name,
+  String $admin_password   = $gernox_keycloak::admin_password,
 
-  String $db_user            = $gernox_keycloak::db_user,
-  String $db_password        = $gernox_keycloak::db_password,
-  String $db_name            = $gernox_keycloak::db_name,
+  String $db_user          = $gernox_keycloak::db_user,
+  String $db_password      = $gernox_keycloak::db_password,
+  String $db_name          = $gernox_keycloak::db_name,
+  String $db_host          = $gernox_keycloak::db_host,
+  Integer $db_port         = $gernox_keycloak::db_port,
 
-  String $keycloak_version   = $gernox_keycloak::keycloak_version,
-  String $postgresql_version = $gernox_keycloak::postgresql_version,
+  String $keycloak_version = $gernox_keycloak::keycloak_version,
 
-  String $log_level          = $gernox_keycloak::log_level,
+  String $log_level        = $gernox_keycloak::log_level,
 ) {
   $docker_environment = [
     'DB_VENDOR=POSTGRES',
-    'DB_ADDR=keycloak-postgres',
-    'DB_PORT=5432',
+    "DB_ADDR=${db_host}",
+    "DB_PORT=${db_port}",
     "DB_DATABASE=${db_name}",
     "DB_USER=${db_user}",
     "DB_PASSWORD=${db_password}",
@@ -60,20 +59,6 @@ class gernox_keycloak::server::run (
     action  => 'accept',
   }
 
-  ::docker::run { 'keycloak-postgres':
-    image                 => "postgres:${postgresql_version}",
-    volumes               => [
-      '/srv/docker/keycloak/postgresql/data:/var/lib/postgresql/data',
-    ],
-    net                   => $network_name,
-    health_check_interval => 30,
-    env                   => [
-      "POSTGRES_USER=${db_user}",
-      "POSTGRES_PASSWORD=${db_password}",
-      "POSTGRES_DB=${db_name}",
-    ],
-  }
-
   ::docker::run { 'keycloak':
     image                 => "jboss/keycloak:${keycloak_version}",
     env                   => $docker_environment,
@@ -82,8 +67,5 @@ class gernox_keycloak::server::run (
       "${http_port}:8080",
     ],
     net                   => $network_name,
-    depends               => [
-      'keycloak-postgres',
-    ],
   }
 }
